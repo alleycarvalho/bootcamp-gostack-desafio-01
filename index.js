d23175b4ffc21cc353aeb9f537ebbb07fb12fd66
Alley M. Carvalho <alleycarvalho@gmail.com>
2019-10-15 00:19:08 -0300

    CRUD: projects resource

--- index.js
+++ index.js
@@ -3,6 +3,58 @@ const server = express();
 
 server.use(express.json());
 
-// Routes and Middlewares
+let numberOfRequests = 0;
+const projects = [];
+
+// List projects
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+// Create project
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+// Read project
+server.get('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+
+  return res.json(project);
+});
+
+// Update project
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+// Delete project
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
 
 server.listen(4000);
